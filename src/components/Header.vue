2153120a430cc3c2d33189411ff6534589c806b5
Cinzia Pulvirenti <cinzia2092@hotmail.it>
2023-03-03 16:28:43 +0100

    Feat creato header

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <header class="main-header">
+      <div class="container">
+        <h1>{{ title }}</h1>
+      </div>
+  
+    </header>
+  </template>
+  
+  <script>
+    export default {
+      data() {
+        return {
+          title: 'Yu-Gi-Oh Api'
+        }
+      }
+      
+    }
+  </script>
+  
+  <style lang="scss" scoped>
+  .main-header {
+    text-align: center;
+    font-size: 2rem;
+    padding: 1rem 0;
+  }
+  </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,50 @@
+<template>
+    <main class="main">
+        <div class="container">
+            <ul class="card">
+                <li>
+                    <img src="" alt="immagine">
+                    <p>nome</p>
+                    <p>tipo</p>
+                </li>
+            </ul>
+        </div>
+
+    </main>
+</template>
+  
+<script>
+import axios from 'axios'
+import store from './store'
+
+
+export default {
+    data() {
+        return {
+            store,
+        }
+    },
+    methods: {
+        fetchCard() {
+            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php')
+                .then((res) => {
+                    console.log(res.data)
+                })
+
+        }
+    }
+}
+
+</script>
+  
+<style lang="scss" scoped>
+.main {
+    background-color: darksalmon;
+}
+
+.card {
+    display: grid;
+    gap: 20px;
+    grid-template-columns: repeat(5, 1fr);
+}
+</style>
