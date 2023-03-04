f7562dd4d1766939074464085604c41aa4f0eea8
Cinzia Pulvirenti <cinzia2092@hotmail.it>
2023-03-04 20:37:58 +0100

    Feat Creato componente Card

---
+++ src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+    <li>
+        <img :src="card.card_images[0].image_url" alt="immagine">
+        <p>{{ card.name }}</p>
+        <p>{{ card.type }}</p>
+    </li>
+</template>
+  
+<script>
+
+
+export default {
+    props: {
+        card: {
+            type: Object,
+            required: true
+        }
+    }
+}
+
+</script>
+  
+<style lang="scss" scoped>
+
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,11 +2,12 @@
     <main class="main">
         <div class="container">
             <ul class="card">
-                <li>
-                    <img src="" alt="immagine">
-                    <p>nome</p>
-                    <p>tipo</p>
-                </li>
+                <Card v-for="card in store.cards" :key="card.id" :card="card"/>
+                <!-- <li v-for="card in store.cards" :key="card.id">
+                    <img :src="card.card_images[0].image_url" alt="immagine">
+                    <p>{{ card.name }}</p>
+                    <p>{{ card.type }}</p>
+                </li> -->
             </ul>
         </div>
 
@@ -16,9 +17,12 @@
 <script>
 import axios from 'axios'
 import store from './store'
-
+import Card from './Card.vue'
 
 export default {
+    components: {
+        Card
+    },
     data() {
         return {
             store,
@@ -26,13 +30,18 @@ export default {
     },
     methods: {
         fetchCard() {
-            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php')
+            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
                 .then((res) => {
-                    console.log(res.data)
+                    console.log(res)
+                    this.store.cards = res.data.data
+                    console.log(store, 'store')
                 })
 
         }
-    }
+    },
+    mounted() {
+        this.fetchCard()
+    },
 }
 
 </script>
